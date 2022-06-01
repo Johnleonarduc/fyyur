5fa783962d8caf822d0b768efa266eec8ede8797
UMUNNAKWE, JOHNLEONARD CHINAEMEREM <johnleonarduc@gmail.com>
2022-06-02 00:45:47 +0100

    fix data bug artist detail page @/artist/id

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -310,7 +310,7 @@ def search_artists():
 @app.route('/artists/<int:artist_id>') #COMPLETED
 def show_artist(artist_id):
   # shows the artist page with the given artist_id
-  artist = Venue.query.get_or_404(artist_id)
+  artist = Artist.query.get_or_404(artist_id)
 
   def get_upcoming_shows():
     upcoming = []
@@ -348,11 +348,10 @@ def show_artist(artist_id):
     "genres": artist.genres,
     "city": artist.city,
     "state": artist.state,
-    "address": artist.address,
     "phone": artist.phone,
     "website_link": artist.website_link,
     "facebook_link": artist.facebook_link,
-    "seeking_talent": artist.seeking_talent,
+    "seeking_venue": artist.seeking_venue,
     "seeking_description": artist.seeking_description,
     "image_link": artist.image_link,
     "past_shows":get_past_shows(),
