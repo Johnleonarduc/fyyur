f70d07ab27cb1565d921d731ad3007636d316a75
UMUNNAKWE, JOHNLEONARD CHINAEMEREM <johnleonarduc@gmail.com>
2022-06-02 02:23:41 +0100

    Edit config.py to remove TODO comment

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -9,5 +9,5 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
+#IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:7856Julcd07@localhost:5432/fyyur'
