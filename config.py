b391f8bee1c78ba25bd22aca05f73385647ea1ac
Johnleonarduc <johnleonarduc@gmail.com>
2023-10-11 09:01:48 +0100

    update config and readme files

--- config.py
+++ config.py
@@ -11,6 +11,11 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Connect to the database
 
-
+DB_NAME = os.getenv("DB_NAME", 'fyyur')
+DB_USER = os.getenv("DB_USER", 'postgres')
+DB_PASSWORD = os.getenv("DB_PASSWORD", 'your_password')
+DB_HOST = os.getenv("DB_HOST", 'localhost')
+DB_PORT = os.getenv("DB_PORT", '5432')
 #IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:7856Julcd07@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
